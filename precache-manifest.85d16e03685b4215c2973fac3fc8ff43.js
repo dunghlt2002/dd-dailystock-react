aad2fbcd5e26c3681b968ee4321ee3396bed5d84
TriDung <dunghlt2002@gmail.com>
2020-08-18 20:57:14 -0400

    Updates

---
+++ precache-manifest.85d16e03685b4215c2973fac3fc8ff43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7246a9a0c1f798856073993d0868b34",
+    "url": "/dd-dailystock-react/index.html"
+  },
+  {
+    "revision": "1d5235f12bfb576f9d5c",
+    "url": "/dd-dailystock-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7b531d2304554427d19",
+    "url": "/dd-dailystock-react/static/css/main.0864afc1.chunk.css"
+  },
+  {
+    "revision": "1d5235f12bfb576f9d5c",
+    "url": "/dd-dailystock-react/static/js/2.863f0a2f.chunk.js"
+  },
+  {
+    "revision": "e4f894af3c0077a98fb36252d2e84892",
+    "url": "/dd-dailystock-react/static/js/2.863f0a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7b531d2304554427d19",
+    "url": "/dd-dailystock-react/static/js/main.6fdca287.chunk.js"
+  },
+  {
+    "revision": "0217d8063f6fb2e28e74",
+    "url": "/dd-dailystock-react/static/js/runtime-main.136609fd.js"
+  }
+]);
